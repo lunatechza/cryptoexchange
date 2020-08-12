6f5ab3c012aa9adeaa86e5c7dc1c400401f04412
Jason Eybers <jasonheybers@gmail.com>
2020-08-12 14:40:10 +0200

    CP-2517 | Update Coingecko API - latest api and use correct volume amount

--- lib/cryptoexchange/exchanges/chainex/market.rb
+++ lib/cryptoexchange/exchanges/chainex/market.rb
@@ -3,6 +3,10 @@ module Cryptoexchange::Exchanges
     class Market < Cryptoexchange::Models::Market
       NAME = 'chainex'
       API_URL = 'https://api.chainex.io'
+
+      def self.trade_page_url(args={})
+        "https://chainex.io/#{args[:base]}/#{args[:target]}"
+      end
     end
   end
 end
